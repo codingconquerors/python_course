3b83f37570159f0d933361e1d5dc54885e4374e8
codingconquerors <codingconquerors@gmail.com>
2024-06-03 17:38:53 -0400

    pytest

--- conditional_stmts/example.py
+++ conditional_stmts/example.py
@@ -9,3 +9,16 @@ elif x >= 60:
     print("Grade: D")
 else:
     print("Grade: F")
+
+x = 10
+if x > 0:
+    if x % 2 == 0:
+        print("x is a positive even number")
+    else:
+        print("x is a positive odd number")
+else:
+    print("x is not a positive number")
+
+x = 10
+result = "Even" if x % 2 == 0 else "Odd"
+print(result)
---
+++ loops/example.py
@@ -0,0 +1,17 @@
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+
+for i in range(5):
+    print(i)
+
+
+i = 0   # starting point 1.
+while i < 20:  # checking the condition 2.
+    print("while loop output ",i)
+    i += 1    # increasing the value of index var as 1 3.
+
+i = 20   # starting point 1.
+while i > 0:  # checking the condition 2.
+    print("while loop output ",i)
+    i -= 1    # decreasing the value of index var as 1 3.
